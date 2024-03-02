6bdce91be9d3d43267175f528b76515248a56817
Sayan Das <sayandas61668@gmail.com>
2024-03-02 12:11:45 +0530

    first commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
     <div className="flex justify-center items-center flex-col py-10">
+      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
       <h1 className="text-7xl font-bold pb-4">Master the Art of Music</h1>
-      <p className=" w-[35%] text-center leading-6">
+      <p className=" w-[35%] text-center leading-6 mb-5">
         Drive into our conperehantion music cource and Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsum tempore itaque dolores nulla neque, necessitatibus beatae culpa est.
         Voluptate, sayanis good
       </p>
-      <button classNamel="mt-4">
+
+      <Button borderRadius="1.75rem" className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
         <Link href={"#"}>Courses</Link>
-      </button>
+      </Button>
     </div>
   );
 };
